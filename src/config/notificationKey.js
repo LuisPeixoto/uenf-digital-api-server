35aa2ea9e4218dd57e260dd61b2f0b657a5db268
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-07-25 15:05:52 -0300

    fix send notification

--- src/config/notificationKey.js
+++ src/config/notificationKey.js
@@ -1,5 +1,5 @@
 module.exports = {
     notification: {
-        key: process.env.NOTIFICATION_KEY
+        key: "AAAAQ5HeKhQ:APA91bGHcXwvw1t0ylMDOIAS-7zg3ArI2-IbaTFTl9blBKJ7mnFe0xjj6r5GIWKmvEes8yriu5wPn2Cpo-7HFAVx5RhLgzpez6sHquyjB3zvmDpS6rj_grlu0dXFydg8e3reUpbqR8Ku"
     }
 }
